393ebc0eb85358637815b69faebd36f632291e03
Richard Guay <raguay@customct.com>
2018-02-15 11:39:43 +0700

    Updated for running editors directly

--- openwitheditor/__init__.py
+++ openwitheditor/__init__.py
@@ -1,21 +1,30 @@
-from fman import DirectoryPaneCommand, show_alert, load_json
+from fman import DirectoryPaneCommand, show_alert
 import os
 from fman.url import as_human_readable
-from fman.url import as_url
 
 
-class BbOpenWithEditor(DirectoryPaneCommand):
+class MyOpenWithEditor(DirectoryPaneCommand):
     def __call__(self, url=None):
         selected_files = []
-        scriptLoc = load_json("OpenWithEditor.json")["scriptLoc"]
-        if url is None:
-            selected_files = self.pane.get_selected_files()
-            if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
-                if len(selected_files) == 0 and self.get_chosen_files():
-                    selected_files.append(self.get_chosen_files()[0])
-                    for file in selected_files:
-                        os.system("'" + scriptLoc + "' 'file' '" + as_human_readable(file) + "' &")
-                else:
-                    show_alert("No files or directories selected")
+        editor = None
+        with open(os.path.expanduser('~') + "/.myeditorchoice", 'r') as content_file:
+            editor = content_file.read().strip()
+
+        if (editor is not None) and (editor != ''):
+            if url is None:
+                selected_files = self.pane.get_selected_files()
+                if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
+                    if len(selected_files) == 0 and self.get_chosen_files():
+                        selected_files.append(self.get_chosen_files()[0])
+                        for file in selected_files:
+                            editFile(editor, file)
+                    else:
+                        show_alert("No files or directories selected")
+            else:
+                editFile(editor, url)
         else:
-            os.system("'" + scriptLoc + "' 'file' '" + as_human_readable(url) + "' &")
+            show_alert("Editor not configured.")
+
+
+def editFile(editor, file):
+    os.system("/usr/bin/open -a '" + editor + "' '" + as_human_readable(file) + "' &")
